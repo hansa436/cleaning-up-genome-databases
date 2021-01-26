5791d1de3d74573c0b9c1526652bd3b9385bce1c
hansa436 <74279158+hansa436@users.noreply.github.com>
2021-01-27 11:30:44 +1300

    Add files via upload
    Chr17 SNP extractions for regions of interest

--- CHR17SNPs.sh
+++ CHR17SNPs.sh
@@ -6,15 +6,16 @@
 #START= Start position
 #END= End position
 
-STEP=5
+STEP=20
 WS=75
 CHROM=17
 START=59999
 END=81195210
 
+#set -xv
+
 #loop
 rm CHR17SNPs.csv
-#set -xv
 printf '%s\n' 'Start, SNPS, PHASTCONS' >> CHR17SNPs.csv
 while [ $START -lt $END ]
 	do
@@ -22,9 +23,9 @@ while [ $START -lt $END ]
 	#if [[ $( echo $PHASTCONS | tr -cd "e" | wc -c ) == "1" ]] ; then PHASTCONS=0 ; else : ; fi
 	[[ "$PHASTCONS" == *"e"* ]] && PHASTCONS=0
 	#bc is needed to fix floating numbers
-		if (( $( echo "$PHASTCONS > 0.74999999" | bc -l ) ))
+		if (( $( echo "$PHASTCONS > 0.4999999" | bc -l ) ))
 		then 
-		SNPS=$(tabix -h gnomad.genomes.r2.1.1.sites.17.vcf.bgz 17:$START-$(( $START + $WS )) | grep -v "#" | cut -f 2| sort| wc -l)
+		SNPS=$(tabix -h gnomad.genomes.v3.1.sites.chr17.vcf.bgz chr17:$START-$(( $START + $WS )) | grep -v "#\|insertion\|deletion" | cut -f 2 | sort | wc -l )
 		echo $START, $SNPS, $PHASTCONS >> CHR17SNPs.csv
 		else
 		:
